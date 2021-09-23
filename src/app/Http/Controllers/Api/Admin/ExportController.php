c0212d7a6449a081b277d76d1426562769b6f557
taeung123 <youtanlai@gmail.com>
2021-09-23 14:32:15 +0700

    add api export query excel

--- src/app/Http/Controllers/Api/Admin/ExportController.php
+++ src/app/Http/Controllers/Api/Admin/ExportController.php
@@ -5,15 +5,20 @@ namespace VCComponent\Laravel\Export\Http\Controllers\Api\Admin;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use VCComponent\Laravel\Export\Contracts\Exportable;
+//use VCComponent\Laravel\Export\Contracts\Exportable;
 use VCComponent\Laravel\Vicoders\Core\Controllers\ApiController;
-use VCComponent\Laravel\Export\Services\Export\Export;
+//use VCComponent\Laravel\Export\Services\Export\Export;
 use VCComponent\Laravel\Export\Traits\ExportTrait;
 use VCComponent\Laravel\Export\Validators\ExportValidator;
 use VCComponent\Laravel\Export\Repositories\ExportsQueryRepository;
-class ExportController extends ApiController implements Exportable
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\Exportable;
+use VCComponent\Laravel\Export\Exports\Export;
+use Maatwebsite\Excel\Facades\Excel;
+use Carbon\Carbon;
+
+class ExportController extends ApiController
 {
-    use ExportTrait;
 
     protected $validator;
 
@@ -28,27 +33,27 @@ class ExportController extends ApiController implements Exportable
             );
         }
     }
-    public function export($slug, $id, Request $request)
+    public function export($slug, Request $request)
     {
-        if($id == null){
-            throw new Exception("id không hợp lệ");
-        }
-        $label = $request->label ? $request->label: "export";
-        $data = $this->repository->findBySlug($slug);
-        $trans = [":id" => "'" . $id . "'"];
-        $tring_query = strtr($data->query, $trans);
-        //$tring_query = "SELECT * FROM `contact_form_values` where `contact_form_id`='2'";
-        $data_export  = $this->getDataExport($tring_query);
-        if($data_export == null){
+        $export_query = $this->repository->findBySlug($slug);
+        if ($export_query == null) {
             throw new Exception("Dữ liệu không tồn tại");
         }
-        $args            = [
-            'data'      => $data_export,
-            'label'     => $label,
-            'extension' => "xlsx",
-        ];
-        $export = new Export($args);
-        $url    = $export->export();
-        return $this->response->array(['url' => $url]);
+        $data_request = $request->all();
+        $trans = [];
+        foreach ($data_request as $item => $val) {
+            $trans[":" . $item] = $val;
+        }
+        $tring_query = strtr($export_query->query, $trans); //dd($tring_query);
+        try {
+            $data = DB::select($tring_query);
+        } catch (Exception $e) {
+            throw new Exception("Dữ liệu không tồn tại");
+        }
+        if ($data == null) {
+            throw new Exception("Dữ liệu không tồn tại");
+        }
+        $label = "export_".$slug."_".date('Y_m_d');
+        return Excel::download(new Export($data), $label.'.xlsx');
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -4,7 +4,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
     $api->group(['prefix' => 'admin'], function ($api) {
-        $api->get('export/{slug}/{id}', 'VCComponent\Laravel\Export\Http\Controllers\Api\Admin\ExportController@export');
+        $api->get('export/{slug}', 'VCComponent\Laravel\Export\Http\Controllers\Api\Admin\ExportController@export');
 
     });
 });
